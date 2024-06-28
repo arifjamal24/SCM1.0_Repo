e562be64794f21182881399d0a12024f3a562815
arifjamal <98831458+arifjamal24@users.noreply.github.com>
2024-06-28 21:20:06 +0530

    lecture 45
    server side validation

--- SmartContactManager/src/main/java/com/scm/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/scm/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.scm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.scm.entities.User;
 import com.scm.helper.Message;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -40,13 +42,19 @@ public class HomeController {
 	}
 	
 	@PostMapping("/doRegister")
-	public String registerUser(@ModelAttribute User user,
+	public String registerUser(@Valid @ModelAttribute User user,BindingResult result,
 			@RequestParam(value="agreement",defaultValue = "false") boolean agreement,Model model,
 			HttpSession session) {
 		try {
 			if(!agreement) {
 				System.out.println("You have not agreed terms and conditions");
-				throw new Exception("You have not agreed terms and conditions");
+				throw new Exception("\nYou have not agreed terms and conditions");
+			}
+			
+			if(result.hasErrors()){
+				System.out.println("result :"+result.toString());
+				model.addAttribute("user",user);
+				return "signup";
 			}
 			user.setRole("ROLE_USER");
 			user.setEnabled(true);
@@ -55,7 +63,7 @@ public class HomeController {
 			System.out.println("agreement: "+agreement);
 			System.out.println("model: "+user);
 			
-			User result = this.userRepository.save(user);
+			 this.userRepository.save(user);
 			
 			model.addAttribute("user", new User());
 			session.setAttribute("message", new Message("Successfully Registered !!","alert-success"));
@@ -63,7 +71,7 @@ public class HomeController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			model.addAttribute("user", user);
-			session.setAttribute("message", new Message("Something went wrong"+e.getMessage(),"alert-danger"));
+			session.setAttribute("message", new Message("Something went wrong."+e.getMessage(),"alert-danger"));
 			return "signup";
 		}
 		
--- SmartContactManager/src/main/java/com/scm/entities/User.java
+++ SmartContactManager/src/main/java/com/scm/entities/User.java
@@ -3,6 +3,9 @@ package com.scm.entities;
 import java.util.*;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class User {
@@ -10,8 +13,12 @@ public class User {
 @Id
 @GeneratedValue(strategy = GenerationType.AUTO)
 private int id;
+
+@NotBlank(message="name field is required !!")
+@Size(min=2, max=20,message="min 2 and max 20 characters are allowed")
 private String name;
 @Column(unique = true)
+@Email(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message="invalid email!!")
 private String email;
 private String password;
 private String role;

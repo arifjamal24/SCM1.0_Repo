4c04007df1bd0a3fe57881b3c7d575e7c596a929
arifjamal <98831458+arifjamal24@users.noreply.github.com>
2024-08-06 19:21:09 +0530

    lecture 55
    image uploading

--- SmartContactManager/src/main/java/com/scm/controller/UserControler.java
+++ SmartContactManager/src/main/java/com/scm/controller/UserControler.java
@@ -1,15 +1,22 @@
 package com.scm.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.scm.dao.UserRepository;
 import com.scm.entities.Contact;
@@ -45,15 +52,37 @@ public class UserControler {
 	}
 	
 	@PostMapping("/addContact")
-	public String addContactAction(@ModelAttribute Contact contact, Principal principal) {
+	public String addContactAction(@ModelAttribute Contact contact,
+			@RequestParam("profileImage") MultipartFile file, Principal principal) {
+		try {
 		var userId = principal.getName();
 		var obj = this.userRepository.getUsersByUserName(userId);
+		
+		// proccessing and uploading file
+		if(!file.isEmpty()) {
+			contact.setImage(file.getOriginalFilename());
+			
+			File saveFile = new ClassPathResource("/static/img/contact").getFile();
+			System.out.println(saveFile);
+			System.out.println(saveFile.getAbsolutePath());
+		var path = Paths.get(saveFile.getAbsolutePath()+File.pathSeparator+file.getOriginalFilename());
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+		System.out.println("Images is uploaded");
+		}
+		else {
+			System.out.println("file is empty");
+		}
+		
 		Users user = obj.get();
 		contact.setUser(user);
 		user.getContacts().add(contact);
 		this.userRepository.save(user);
 		System.out.println(contact);
 		System.out.println("added to databnase");
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		return "general/addContact";
 	}
 

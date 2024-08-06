d25125e5dd36a08735766d21ea7dc97b186683d4
arifjamal <98831458+arifjamal24@users.noreply.github.com>
2024-08-06 13:14:01 +0530

    lecture 53 p1

--- SmartContactManager/src/main/java/com/scm/controller/UserControler.java
+++ SmartContactManager/src/main/java/com/scm/controller/UserControler.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.scm.dao.UserRepository;
+import com.scm.entities.Contact;
 import com.scm.entities.Users;
 
 @Controller
@@ -17,14 +20,27 @@ public class UserControler {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	@RequestMapping("/index")
-	public String dashboard(Model m,Principal p) {
+
+	@ModelAttribute
+	public void addCommonData(Model m, Principal p) {
 		// using Principal we get username in out case we get emailid as a username
 		String userName = p.getName();
 		Optional<Users> user = userRepository.getUsersByUserName(userName);
 		var u = user.get();
-		m.addAttribute("userDetail", u);
+		m.addAttribute("userDetail", u); 
+	}
+
+	@RequestMapping("/index")
+	public String dashboard(Model m) {
+		m.addAttribute("title","User Dashboard");
 		return "general/userDashboard";
 	}
+
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model m) {
+		m.addAttribute("title","Add Contact");
+		m.addAttribute("contact", new Contact());
+		return "general/addContact";
+	}
+
 }

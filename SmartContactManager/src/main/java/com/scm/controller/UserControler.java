0166eb3a49560785fd2c90a80faf79c3eb7f15c7
arifjamal <98831458+arifjamal24@users.noreply.github.com>
2024-08-06 09:47:46 +0530

    lecture 51
    creating user dashboard navigation bar

--- SmartContactManager/src/main/java/com/scm/controller/UserControler.java
+++ SmartContactManager/src/main/java/com/scm/controller/UserControler.java
@@ -1,15 +1,30 @@
 package com.scm.controller;
 
+import java.security.Principal;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.dao.UserRepository;
+import com.scm.entities.Users;
 
 @Controller
 @RequestMapping("/user")
 public class UserControler {
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	@RequestMapping("/index")
-	public String dashboard() {
-		
+	public String dashboard(Model m,Principal p) {
+		// using Principal we get username in out case we get emailid as a username
+		String userName = p.getName();
+		Optional<Users> user = userRepository.getUsersByUserName(userName);
+		var u = user.get();
+		m.addAttribute("userDetail", u);
 		return "general/userDashboard";
 	}
 }
